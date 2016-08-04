7cedb8d1bd7b4b5be97d68f7e554ec3b443cccfa
kenny.k <we.want.to.know.that@gmail.com>
2016-08-04 16:46:15 +0900

    WebStorm 커밋 테스트

--- Chapter3/test.js
+++ Chapter3/test.js
@@ -1,6 +1,10 @@
 /**
  * Created by kenny.k on 2016. 8. 3..
  */
-alert ( "test js1!!" );
-alert ( "test js2!!" );
-alert ( "test js3!!" );
+alert("test js1!!");
+alert("test js2!!");
+alert("test js3!!");
+
+var object = new Object();
+
+console.log(object);
