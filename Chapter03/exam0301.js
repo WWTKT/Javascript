3792dda2d5ec7035a63f9ae164623058101b5a1c
Tony <jipark74@gmail.com>
2016-08-04 09:56:45 +0900

    첫번째 커밋

--- Chapter03/exam0301.js
+++ Chapter03/exam0301.js
@@ -1,3 +1,32 @@
 /**
  * Created by tony on 2016. 8. 4..
  */
+// 숫자 타입
+var intNum = 10;
+var floatNum = 0.1;
+
+// 문자열 타입
+var singleQuoteStr = 'single quote string';
+var doubleQuoteStr = "double quote string";
+var singChar = 'a';
+
+// 불린 타입
+var boolVar = true;
+
+// undefined type
+var emptyVar;
+
+// null type
+var nullVar = null;
+
+
+console.log(
+    typeof intNum,
+    typeof floatNum,
+    typeof singleQuoteStr,
+    typeof doubleQuoteStr,
+    typeof singChar,
+    typeof boolVar,
+    typeof emptyVar,
+    typeof nullVar
+)
